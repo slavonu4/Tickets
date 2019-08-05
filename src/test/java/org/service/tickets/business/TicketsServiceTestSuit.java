283d141b12cd4b32b1a97d59343662a50c0527b0
Вячеслав <slavonu4@gmail.com>
2019-08-05 21:15:32 +0300

    Added unit tests for tickets controller and service

---
+++ src/test/java/org/service/tickets/business/TicketsServiceTestSuit.java
@@ -0,0 +1,62 @@
+package org.service.tickets.business;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.service.tickets.domain.dao.TicketsDAO;
+import org.service.tickets.domain.model.Ticket;
+import org.service.tickets.presentation.dto.NewTicketDTO;
+import org.service.tickets.presentation.exception.ValidationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.validation.BindingResult;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class TicketsServiceTestSuit {
+    @MockBean
+    private TicketsDAO dao;
+
+    @Autowired
+    private TicketsService ticketsService;
+
+    @Test(expected = ValidationFailedException.class)
+    public void preventCreationOfTicketWithFailedValidation() throws ValidationFailedException {
+        var ticket = new NewTicketDTO("test", null);
+        var bindingResultMock = mock(BindingResult.class);
+        when(bindingResultMock.hasErrors()).thenReturn(true);
+
+        ticketsService.createTicket(ticket, bindingResultMock);
+    }
+
+    @Test
+    public void createValidTicket() throws ValidationFailedException {
+        var ticket = new NewTicketDTO("test", LocalDateTime.now());
+        var ticketModelCaptor = ArgumentCaptor.forClass(Ticket.class);
+        var bindingResultMock = mock(BindingResult.class);
+        var newTicketId = 1L;
+
+
+        when(bindingResultMock.hasErrors()).thenReturn(false);
+        when(dao.save(ticketModelCaptor.capture())).thenAnswer(invocation ->  {
+                var model = ticketModelCaptor.getValue();
+                model.setId(newTicketId);
+
+                return model;
+        });
+
+        var result = ticketsService.createTicket(ticket, bindingResultMock);
+
+        Assert.assertEquals(newTicketId, result.longValue());
+    }
+}
